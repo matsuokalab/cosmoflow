d13e8671890f0408ab163ef5a7f3dcc3f847de18
aca10027xu <aca10027xu@es2.abci.local>
2020-09-11 21:40:32 +0900

    disable progressbar

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
     path_data = "/groups1/gac50489/datasets/cosmoflow/cosmoUniverse_2019_05_4parE_tf_small"
     path_data = "/groups1/gac50489/datasets/cosmoflow_full/cosmoUniverse_2019_05_4parE_tf"
     data_module = CFDataModule(path_data, batch_size=2)
-    # wandb_logger = WandbLogger(project="cosmoflow")
+    wandb_logger = WandbLogger(project="cosmoflow")
     early_stop_callback = EarlyStopping(
         monitor='val_loss',
         min_delta=0.0001,
@@ -59,7 +59,8 @@ def main():
         distributed_backend="horovod",
         replace_sampler_ddp=False,
         early_stop_callback=early_stop_callback,
-        # logger=wandb_logger,
+        logger=wandb_logger,
+        show_progress_bar=False,
     )
     # print("tainer created")
 
